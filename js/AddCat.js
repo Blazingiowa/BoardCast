2c844035dbdfa1001b99bbfeaca015800ac81f5f
Blazingiowa <yasutaka913@gmail.com>
2020-06-02 16:10:20 +0900

    UIのみ

--- js/AddCat.js
+++
@@ -1,5 +0,0 @@
-function AddCategory() {
-    var AddCat = document.getElementById("add_cat");
-
-    //AddCat.insertAdjacentHTML('afterbegin', '<span class="channel-name" id="add_cat">TestChannel</span>')
-}
