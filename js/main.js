4f0065a539fef620718e63ef9f2cbde55dd9e086
tomcattomtom <ridezeko@eay.jp>
2020-06-08 15:15:08 +0900

    waithi

--- js/main.js
+++ js/main.js
@@ -36,3 +36,30 @@ $(function () {
         },anitime);
     });
 });
+
+
+let button = document.getElementsByClassName('compose');
+
+document.addEventListener('click', function() {
+    console.log("yaa");
+    const mysql = require('mysql');
+        const con = mysql.createConnection({
+            host: '192.168.10.7',
+            user: 'root',
+            password: 'ncc_NCC2020',
+            database: 'testu22'
+            
+        });
+    
+        con.connect();
+        let sql1= "insert into memberinfo values('masuda','ohayou2@gmail.com',3345);";
+        
+        con.query(sql1,(err,rows,fields)=>{
+        
+            if(err) throw err;
+            console.log('userテーブル：',rows);
+        
+        });
+        con.end();
+        
+});
