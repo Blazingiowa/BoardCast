f76f0e01af74b52dd064a1b8bd575a281598fe71
Blazingiowa <yasutaka913@gmail.com>
2020-07-08 14:21:02 +0900

    formを大幅刷新

---
+++ js/form_socket.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+    OnSubmitEvent();
+}
+
+function OnSubmitEvent() {
+    $(function () {
+        $('#taihi').click(function () {
+            window.location.href = 'index.html'
+        })
+    });
+}
